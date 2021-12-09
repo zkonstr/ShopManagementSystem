c7a4baf113edbd7ca884a196e322ef6e3e149b5a
Konstantin <zkonstr@gmail.com>
2021-12-09 15:59:21 +0300

    /fix 2

--- ShopManagementSystem/StockUpdate.Designer.cs
+++ ShopManagementSystem/StockUpdate.Designer.cs
@@ -99,9 +99,9 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(15, 251);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.Size = new System.Drawing.Size(71, 20);
             this.label4.TabIndex = 44;
-            this.label4.Text = "label4";
+            this.label4.Text = "Quantity";
             // 
             // ProductName
             // 
